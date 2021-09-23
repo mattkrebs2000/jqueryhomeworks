a2b865973d789c169db5c7e5d3ac024ef804ae75
mattkrebs1974 <mattkrebsemail@gmail.com>
2021-09-22 20:08:40 -0400

    console.log

--- Week3Folder/example.js
+++ Week3Folder/example.js
@@ -1,9 +1,11 @@
 
 const body = document.body;
+console.log(body);
 const div = document.createElement("div");
-
+console.log(div);
 div.innerText = "hello";
 div.classList.add("trythis");
 
 body.prepend(div);
+
   
