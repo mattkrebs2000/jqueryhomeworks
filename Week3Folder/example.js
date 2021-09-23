3f07b13392403e236c1353f1ff0dfa61fdf9a315
mattkrebs1974 <mattkrebsemail@gmail.com>
2021-09-22 20:23:12 -0400

    simple edit

--- Week3Folder/example.js
+++ Week3Folder/example.js
@@ -3,8 +3,10 @@ const body = document.body;
 console.log(body);
 const div = document.createElement("div");
 console.log(div);
+console.log(div.innerText);
 div.innerText = "hello";
 div.classList.add("trythis");
+console.log(div.innerText);
 
 body.prepend(div);
 
