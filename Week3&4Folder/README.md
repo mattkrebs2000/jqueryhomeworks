a8e9aa7c8230492deef9fea00af4757497ecc226
mattkrebs1974 <mattkrebsemail@gmail.com>
2021-09-25 14:19:40 -0400

    new divs

--- Week3&4Folder/README.md
+++ Week3&4Folder/README.md
@@ -1,1 +1,7 @@
-Good Youtube Video for Dynamic poplation of the Dom --- https://youtu.be/y17RuWkWdn8
+Good Youtube Video for Dynamic poplation of the Dom --- 
+
+https://youtu.be/y17RuWkWdn8
+
+https://www.youtube.com/watch?v=umHdBuTw98c
+
+https://www.youtube.com/watch?v=VsXCK_2DJzA
--- Week3&4Folder/example.js
+++ Week3&4Folder/example.js
@@ -1,13 +1,29 @@
 
 const body = document.body;
 console.log(body);
-const div = document.createElement("div");
-console.log(div);
-console.log(div.innerText);
-div.innerText = "hello";
-div.classList.add("trythis");
-console.log(div.innerText);
+const div1 = document.createElement("span");
+const div2 = document.createElement("span");
+const div3 = document.createElement("span");
+const button1 = document.createElement("button");
+console.log(div1);
+console.log("does anything show up here?", div1.innerText);
+console.log(button1)
 
-body.prepend(div);
+div1.innerHTML = "hello ";
+
+div2.innerText = " there";
+
+div3.textContent ="!";
+
+
+button1.name = "joe";
+div1.classList.add("trythis");
+div2.classList.add("trythis");
+div3.classList.add("trythis");
+console.log(div1.innerText);
+
+body.prepend(div1);
+body.append(div2);
+body.append(div3);
 
   
