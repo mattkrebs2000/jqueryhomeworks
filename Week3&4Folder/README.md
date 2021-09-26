3ba2df2eb9646639ef4233df6f0f97e7fbbc2d89
mattkrebs1974 <mattkrebsemail@gmail.com>
2021-09-26 12:13:28 -0400

    week 3/4 assignment

--- Week3&4Folder/README.md
+++ Week3&4Folder/README.md
@@ -5,3 +5,25 @@ https://youtu.be/y17RuWkWdn8
 https://www.youtube.com/watch?v=umHdBuTw98c
 
 https://www.youtube.com/watch?v=VsXCK_2DJzA
+
+
+Week 1 Instructions:
+
+Watch the videos listed above and look over the example (js and html files).
+
+Your goal will be to reproduce the html from website.html but do it through a js file. This means that your html file should only have body tags or a single div tag into which you will dynamically input the rest of the html code. 
+
+Please pay close attention to the following requirements: 
+
+1. You may not use the "innerhtml" property as you can see how easy that would be in "website.js."
+
+2. You must use a forloop of a for each loop to render the menu items (home, about, portfolio etc.)
+
+3. Your website should look exactly like the original version from website.html. 
+
+4. write in atleast 4 comments describing what you are doing. 
+
+5. (For full credit) - you should try to delay the rendering of these menu items so that they appear one after the other.
+
+ 
+Save your work (should be a folder with images folder, js file, html file and css file)to a repository of your own and pass in a link to it when you are done. 
--- Week3&4Folder/example.js
+++ Week3&4Folder/example.js
@@ -4,17 +4,29 @@ console.log(body);
 const div1 = document.createElement("span");
 const div2 = document.createElement("span");
 const div3 = document.createElement("span");
+const list = document.createElement("ul");
+const names = document.getElementById("newId");
+const listofnames = ["water", "air", "gas"]
+console.log(list, names);
 const button1 = document.createElement("button");
 console.log(div1);
 console.log("does anything show up here?", div1.innerText);
-console.log(button1)
+console.log(button1);
 
-div1.innerHTML = "hello ";
+// div1.innerHTML = "hello ";
+div1.innerHTML = "<span>hello </span>"
 
 div2.innerText = " there";
 
 div3.textContent ="!";
 
+
+listofnames.forEach(element => {
+    var node = document.createElement("LI");                 // Create a <li> node
+var textnode = document.createTextNode(element);         // Create a text node
+node.appendChild(textnode);                              // Append the text to <li>
+names.append(node);
+});
 
 button1.name = "joe";
 div1.classList.add("trythis");
@@ -22,8 +34,11 @@ div2.classList.add("trythis");
 div3.classList.add("trythis");
 console.log(div1.innerText);
 
+
+body.prepend(div3);
+body.prepend(div2);
 body.prepend(div1);
-body.append(div2);
-body.append(div3);
+
+
 
   
